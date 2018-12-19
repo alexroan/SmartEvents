559b9c97165ce44fca036f24b024b7580b75dd4b
Alex Roan <alex.roan@hotmail.com>
2018-12-19 22:59:41 +0000

    Added ticket purchasing, not tested yet

--- contracts/SimpleEvent.sol
+++ contracts/SimpleEvent.sol
@@ -8,7 +8,13 @@ contract SimpleEvent {
     uint private tickets;
     //Price of tickets
     uint private price;
+    //Tickets held
+    mapping(address => uint) ticketHolders;
+    //Balances for each account
+    mapping(address => uint) balances;
 
+    //Log on the blockchain that tickets have been bought
+    event LogTicketsBought(address indexed accountAddress, uint tickets, uint price);
 
     //Create a new Event
     constructor(uint _tickets, uint _price) public payable {
@@ -18,11 +24,15 @@ contract SimpleEvent {
         price = _price;
     }
 
-    //Set the price of a ticket in ether
-    function setPrice(uint _newPrice) public returns (uint) {
-        require(msg.sender == owner, "Only the owner of this event can alter the price of tickets");
-        price = _newPrice;
-        return price;
+    //Buy tickets for the event
+    //TODO Test
+    function buyTickets(uint _tickets) public payable returns(uint) {
+        require(tickets > 0, "No tickets available");
+        require(tickets >= _tickets, "Not enough tickets available");
+        require(msg.value >= _tickets * price, "Not enough ether sent to buy that many tickets");
+        tickets -= _tickets;
+        ticketHolders[msg.sender] = _tickets;
+        balances[msg.sender] += msg.value;
     }
 
     //Return price of a ticket in ether
@@ -33,5 +43,17 @@ contract SimpleEvent {
     //Get the number of tickets remaining
     function ticketsRemaining() public view returns(uint) {
         return tickets;
+    }
+
+    //Only owner modifier
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only the owner can do this.");
+        _;
+    }
+
+    //Set the price of a ticket in ether
+    function setPrice(uint _newPrice) public onlyOwner returns (uint) {
+        price = _newPrice;
+        return price;
     }
 }
