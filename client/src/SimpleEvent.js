31e2bd8a91977cb29a7d9cd2cb28b0b2de2d04dd
Alex Roan <alex.roan@hotmail.com>
2018-12-28 16:40:57 +0000

    Figured way of sending the 'from' address with the call to the contract

--- client/src/SimpleEvent.js
+++ client/src/SimpleEvent.js
@@ -5,12 +5,15 @@ class SimpleEvent extends React.Component {
     const { drizzle } = this.props;
     const contract = drizzle.contracts.SimpleEvent;
 
+    console.log(this.props.drizzleState.accounts[4]);
     // let drizzle know we want to watch the `getName` method
     const nameKey = contract.methods["getName"].cacheCall();
     const priceKey = contract.methods["getPrice"].cacheCall();
+    var notOwner = this.props.drizzleState.accounts[4];
+    const balanceKey = contract.methods["getBalance"].cacheCall({from: notOwner});
 
     // save the `dataKey` to local component state for later reference
-    this.setState({ nameKey,priceKey });
+    this.setState({ nameKey,priceKey,balanceKey });
   }
 
   render() {
@@ -18,16 +21,18 @@ class SimpleEvent extends React.Component {
     const { SimpleEvent } = this.props.drizzleState.contracts;
 
     // using the saved `dataKey`, get the variable we're interested in
-    var getName = "loading name"
-    var getPrice = "Loading price"
-    console.log(this.props.drizzleState.accountBalances);
+    var getName = "loading name";
+    var getPrice = "Loading price";
+    var balance = "Loading balance";
+    console.log(this.props);
     if(this.state != null){
         getName = SimpleEvent.getName[this.state.nameKey];
         getPrice = SimpleEvent.getPrice[this.state.priceKey];
+        balance = SimpleEvent.getBalance[this.state.balanceKey];
     }
 
     // if it exists, then we display its value
-    return <p>Event name: {getName && getName.value} Price: {getPrice && getPrice.value}</p>;
+    return <p>Event name: {getName && getName.value} Price: {getPrice && getPrice.value} Balance: {balance && balance.value}</p>;
   }
 }
 
